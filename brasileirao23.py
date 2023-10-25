4f4321d1d5ab2c00ee2d826f4401f5666a390838
betoschneider <roberto.schneider@gmail.com>
2023-10-25 19:11:21 -0300

    resultado por time na barra lateral

--- brasileirao23.py
+++ brasileirao23.py
@@ -45,6 +45,34 @@ st.markdown('###### Um exemplo de dashboard em Python com Streamlit  :sunglasses
 time = st.sidebar.selectbox('Time', tabela_consolidada['time'].unique())
 tabela_consolidada_filtro = tabela_consolidada[(tabela_consolidada['time']==time) & (tabela_consolidada['rodada_projecao']==max_rodada)]
 
+#resultado do filtro
+#resultado por time
+st.sidebar.header(str(time))
+st.sidebar.markdown(
+    '''###### Realizado até a %sª rodada 
+    %sº lugar
+    %sv %se %sd
+    %s pontos 
+    %s%% de aproveitamento
+    %s gols marcados
+    %s gols de saldo
+     ''' % (max_rodada, str(int(tabela_consolidada_filtro['classificacao'].max())),
+             str(int(tabela_consolidada_filtro['vitoria_total'].max())),
+             str(int(tabela_consolidada_filtro['empate_total'].max())),
+             str(int(tabela_consolidada_filtro['derrota_total'].max())),
+             str(int(tabela_consolidada_filtro['pontos_total'].max())),
+             str(round(tabela_consolidada_filtro['aproveitamento_total'].max() * 100, 2)),
+             str(int(tabela_consolidada_filtro['gols_marcados_total'].max())),
+             str(int(tabela_consolidada_filtro['saldo_gols_total'].max()))
+             )
+)
+st.sidebar.markdown(
+        '''###### Projeção na rodada atual
+        %s pontos projetados
+        ''' % (int(tabela_consolidada_filtro['pontos_finais'].max()))
+)
+
+
 #colunas na página
 col1, col2 = st.columns(2)
 col3, col0 = st.columns(2)
@@ -76,32 +104,5 @@ fig2 = px.line(tabela_consolidada, x='rodada_projecao', y='pontos_finais', color
 fig2.update_layout(showlegend=False)
 col2.plotly_chart(fig2, use_container_width=True)
 
-
-#resultado por time
-#st.header(str(time), divider='grey')
-col3.header(str(time))
-col4.markdown(
-    '''###### Realizado até a %sª rodada 
-    %sº lugar
-    %sv %se %sd
-    %s pontos (%s%% de aproveitamento)
-    %s gols marcados
-    %s gols de saldo
-     ''' % (max_rodada, str(int(tabela_consolidada_filtro['classificacao'].max())),
-             str(int(tabela_consolidada_filtro['vitoria_total'].max())),
-             str(int(tabela_consolidada_filtro['empate_total'].max())),
-             str(int(tabela_consolidada_filtro['derrota_total'].max())),
-             str(int(tabela_consolidada_filtro['pontos_total'].max())),
-             str(round(tabela_consolidada_filtro['aproveitamento_total'].max() * 100, 2)),
-             str(int(tabela_consolidada_filtro['gols_marcados_total'].max())),
-             str(int(tabela_consolidada_filtro['saldo_gols_total'].max()))
-             )
-)
-
-col5.markdown(
-        '''###### Projeção na rodada atual
-        %s pontos projetados ao final do campeonato
-        ''' % (int(tabela_consolidada_filtro['pontos_finais'].max()))
-)
-
+#link
 st.link_button('Meu perfil no LinkedIn', 'https://www.linkedin.com/in/robertoschneider/')
